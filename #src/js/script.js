790405797f068f9922a8263dad6fbd673d9a9d8a
boyko1989 <hunwiss89@gmail.com>
2021-07-02 15:36:59 +0400

    Меню-бургер готов

--- #src/js/script.js
+++ #src/js/script.js
@@ -43,7 +43,20 @@ if (isMobile.any()){
    document.body.classList.add('_pc');
 }
 
-// прокрутка при клике
+// Меню бургер
+
+const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
+
+if(iconMenu){   
+   iconMenu.addEventListener("click", function(e){
+      document.body.classList.toggle('_lock');
+      iconMenu.classList.toggle('_active');
+      menuBody.classList.toggle('_active');
+   }); 
+}
+
+// Прокрутка при клике
 
 const menuLinks = document.querySelectorAll(".menu__link[data-goto], .menu__sub-link[data-goto]");
 
@@ -57,6 +70,12 @@ if(menuLinks.length > 0){
       if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)){
          const gotoBlock = document.querySelector(menuLink.dataset.goto);
          const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('.header').offsetHeight;
+
+      if(iconMenu.classList.contains('_active')){
+         document.body.classList.remove('_lock');
+         iconMenu.classList.remove('_active');
+         menuBody.classList.remove('_active');
+      }
 
          window.scrollTo({
             top: gotoBlockValue,
